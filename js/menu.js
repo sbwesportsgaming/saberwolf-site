fe6a2215e81b3301701e61648175ee060db68901
sbwesportsgaming <sbwesportsgaming@gmail.com>
2026-06-06 16:16:24 -0300

    Delete js directory

--- js/menu.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
-
-if (toggle && nav) {
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
-}
